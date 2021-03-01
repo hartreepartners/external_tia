9fd492513ccc17a6de35e320ca04d9e1b387ce92
Salim Fadhley <sfadhley@hartree.com>
2021-03-01 16:29:25 +0000

    Reformatted

--- setup.py
+++ setup.py
@@ -7,18 +7,19 @@ try:
 except ImportError:
     from distutils.core import setup
 
+
 def read_file(path):
-    with open(path, 'r') as f:
+    with open(path, "r") as f:
         return f.read()
 
 
-AUTHOR = 'Brian P Smith'
-AUTHOR_EMAIL = 'brian.p.smith@gmail.com'
-PACKAGE = 'tia3'
-PACKAGE_DESC = 'Toolkit for integration and analysis'
+AUTHOR = "Brian P Smith"
+AUTHOR_EMAIL = "brian.p.smith@gmail.com"
+PACKAGE = "tia3"
+PACKAGE_DESC = "Toolkit for integration and analysis"
 VERSION = tia.__version__
 URL = "https://github.com/bpsmith/tia"
-REQUIRED = ['pandas', 'numpy']
+REQUIRED = ["pandas", "numpy"]
 REQUIRED_FOR_TESTS = []
 
 LONG_DESC = """\
@@ -34,26 +35,33 @@ setuptools.setup(
     install_requires=REQUIRED,
     long_description=LONG_DESC,
     name=PACKAGE,
-    packages=['tia', 'tia.analysis', 'tia.bbg', 'tia.rlab', 'tia.tests',
-        'tia.util', 'tia.analysis.model'],
-    package_dir={'tia': 'tia'},
-    test_suite='tia.tests',
+    packages=[
+        "tia",
+        "tia.analysis",
+        "tia.bbg",
+        "tia.rlab",
+        "tia.tests",
+        "tia.util",
+        "tia.analysis.model",
+    ],
+    package_dir={"tia": "tia"},
+    test_suite="tia.tests",
     tests_require=REQUIRED_FOR_TESTS,
     url=URL,
     version=VERSION,
-    keywords=['bloomberg', 'backtesting', 'technical analysis', 'pdf'],
+    keywords=["bloomberg", "backtesting", "technical analysis", "pdf"],
     download_url=URL,
-    license='BSD (3-clause)',
+    license="BSD (3-clause)",
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Financial and Insurance Industry',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
-        'Topic :: Office/Business :: Financial',
-        'Topic :: Office/Business :: Financial :: Investment',
-        'Topic :: Utilities',
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Financial and Insurance Industry",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Office/Business :: Financial",
+        "Topic :: Office/Business :: Financial :: Investment",
+        "Topic :: Utilities",
     ],
 )
